270e318de0aa653542eb10628148263a8a7e566b
Lukas Vecerka <vecerkalukas18@gmail.com>
2022-04-16 15:08:19 +0200

    create program structure

---
+++ proj2.c
@@ -0,0 +1,38 @@
+// IOS Projekt 2 - Building H2O
+// Lukas Vecerka xvecer30
+// 1 BIT ak. rok 2021/22
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int init(){
+
+}
+
+void clean_everything(){
+
+}
+
+void process_molecule(){
+
+}
+
+void process_oxygen(){
+
+}
+
+void process_hydrogen(){
+
+}
+
+void oxygen_generator(){
+
+}
+
+void hydrogen_generator(){
+
+}
+
+int main(int argc, char **argv){
+
+}
